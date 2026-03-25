0ba10b80bf4c561f1c9f057d55131005bee9d37d
Kayk Lima <165853945+16-09-06@users.noreply.github.com>
2026-03-25 17:43:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'missrose-v7';
+const urlsToCache = [
+
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './logo_missrose.png',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://cdn.jsdelivr.net/npm/sweetalert2@11',
+    'https://unpkg.com/imask',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] App Instalado!');
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Ativação do Service Worker e Limpeza de Caches Antigos
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Ativado');
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('[Service Worker] Removendo cache antigo:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Interceptação de rede: Tenta usar o cache primeiro para velocidade e modo offline
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Ouve mensagens da página (app.js) para pular a espera e ativar a nova versão
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
