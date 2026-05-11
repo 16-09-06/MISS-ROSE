2141470d696f3d0e1980207eb69e1ba152e6760e
Kayk Lima <165853945+16-09-06@users.noreply.github.com>
2026-05-11 10:25:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ async function realizarLogin() {
         });
         const res = await response.text();
 
-        if (res.trim().toLowerCase() === "Autorizado") {    
+        if (res.trim().toLowerCase() === "autorizado") {    
             if (senhaRaw === APP_CONFIG.SENHA_PADRAO) {
                 forcarTrocaSenha(user, efetivarAcesso);
             } else {
